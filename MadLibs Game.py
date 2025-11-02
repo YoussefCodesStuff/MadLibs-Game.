d79373171bdf757652e9d87d0a770b01166e0f19
Youssef Hossam <heyitsyoussef@gmail.com>
2025-11-02 22:02:50 +0300

    Add files via upload

---
+++ MadLibs Game.py
@@ -0,0 +1,23 @@
+# Welcome to Joe's MadLib game :)
+# Let me know if you enjoyed anything about this.
+# Also, I'm going to be using the {time} thing so that's cool.
+import time
+name = input("What is your name?: ")
+Adjective1 = input("Enter an adjective (description): ")
+Noun1 = input("Enter a noun (person, place, or thing): ")
+Adjective2 = input("Enter an adjective (description): ")
+Verb1 = input("Enter a verb (action or state of being): ")
+Adjective3 = input("Enter an adjective (description): ")
+
+
+print(f"Today I went to a(n) {Adjective1} Museum.")
+time.sleep(1)
+print(f"In an exhibit, I saw a {Noun1}")
+time.sleep(1)
+print(f"The {Noun1} was {Adjective2} and {Verb1}")
+time.sleep(1)
+print(f"He was {Adjective3}!")
+print()
+print(f"Thank you for playing, {name}!")
+
+# I'm currently following a guide and these projects just motivate me to continue learning.
